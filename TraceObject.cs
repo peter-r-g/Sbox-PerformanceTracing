1157f23ad254077734ce7624104385fa0581fb25
peter-r-g <petergorman11@gmail.com>
2023-07-19 19:32:42 -0400

    Rename OtherData to MetaData

--- TraceObject.cs
+++ TraceObject.cs
@@ -12,7 +12,7 @@ internal sealed class TraceObject
 	[JsonPropertyName( "traceEvents" )]
 	public ConcurrentBag<TraceEvent> TraceEvents { get; set; } = new();
 	[JsonPropertyName( "otherData" )]
-	public Dictionary<string, object?> OtherData { get; init; } = new()
+	public Dictionary<string, object?> MetaData { get; init; } = new()
 	{
 		{ "perfTracingVersion", "1.0.0" },
 	};
@@ -22,30 +22,30 @@ internal sealed class TraceObject
 	{
 		if ( Game.IsServer )
 		{
-			OtherData.Add( "realm", "server" );
-			OtherData.Add( "serverSteamid", Game.ServerSteamId );
-			OtherData.Add( "isDedicatedServer", Game.IsDedicatedServer );
-			OtherData.Add( "tickRate", Game.TickRate );
+			MetaData.Add( "realm", "server" );
+			MetaData.Add( "serverSteamid", Game.ServerSteamId );
+			MetaData.Add( "isDedicatedServer", Game.IsDedicatedServer );
+			MetaData.Add( "tickRate", Game.TickRate );
 		}
 		else if ( Game.IsClient )
 		{
-			OtherData.Add( "realm", "client" );
-			OtherData.Add( "steamid", Game.SteamId );
-			OtherData.Add( "isServerHost", Game.IsServerHost );
-			OtherData.Add( "tickRate", Game.TickRate );
+			MetaData.Add( "realm", "client" );
+			MetaData.Add( "steamid", Game.SteamId );
+			MetaData.Add( "isServerHost", Game.IsServerHost );
+			MetaData.Add( "tickRate", Game.TickRate );
 		}
 		// Menu.
 		else
 		{
-			OtherData.Add( "realm", "menu" );
+			MetaData.Add( "realm", "menu" );
 		}
 
 		if ( Game.IsDedicatedServer )
 			return;
 
-		OtherData.Add( "isEditorRunning", Game.IsEditor );
-		OtherData.Add( "isHandheld", Game.IsRunningOnHandheld );
-		OtherData.Add( "isVr", Game.IsRunningInVR );
+		MetaData.Add( "isEditorRunning", Game.IsEditor );
+		MetaData.Add( "isHandheld", Game.IsRunningOnHandheld );
+		MetaData.Add( "isVr", Game.IsRunningInVR );
 
 		StartTime = Stopwatch.GetElapsedTime( 0 );
 	}
