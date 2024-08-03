0eb41afe249503120048169b49ef487feb94626a
peter-r-g <petergorman11@gmail.com>
2024-08-03 08:00:06 -0400

    Bump version

--- Code/Tracing.cs
+++ Code/Tracing.cs
@@ -22,7 +22,7 @@ public static class Tracing
 	/// <summary>
 	/// The current version of the Performance Tracing library.
 	/// </summary>
-	public const string Version = "2.0.0";
+	public const string Version = "3.0.0";
 
 	internal static readonly ImmutableArray<string> DefaultCategories = ImmutableArray.Create( "Uncategorized" );
 
